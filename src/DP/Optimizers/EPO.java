fba56c86c0921d3a9ddd0afc32ed0c96a96b4b7a
Jellybeanci <gokselkucuksahin@gmail.com>
2021-01-31 02:41:53 +0300

    JNum için Lisans eklendi.

--- src/DP/Optimizers/EPO.java
+++ src/DP/Optimizers/EPO.java
@@ -106,6 +106,17 @@ public class EPO extends Optimizer
   @Override
   public void run(Space space, Func function, boolean storeBestOnly, Func preEvalution)
   {
+     /*
+     Runs the optimization pipeline.
+       Args:
+           space (Space): A Space object that will be evaluated.
+           function (Function): A Function object that will be used as the objective function.
+           store_best_only (bool): If True, only the best agent of each iteration is stored in History.
+           pre_evaluation (callable): This function is executed before evaluating the function being optimized.
+       Returns:
+           A History object holding all agents' positions and fitness achieved during the task.
+      */
+
 
   }
 }
