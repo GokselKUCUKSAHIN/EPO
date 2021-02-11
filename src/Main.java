41ef75ccce47281f9590b7871c725eeec1e465ec
Jellybeanci <gokselkucuksahin@gmail.com>
2021-02-11 13:25:15 +0300

    Minor fixes.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import DP.Utils.JNum;
+
 public class Main
 {
 
--- src/Test/DP/TestMain.java
+++ src/Test/DP/TestMain.java
@@ -1,7 +1,5 @@
 package Test.DP;
 
-import DP.Math.Random;
-import DP.Optimizers.EPO_MAT;
 import DP.Utils.JNum;
 import Test.DP.Core.UnitTest_Agent;
 import Test.DP.Core.UnitTest_Func;
@@ -14,8 +12,6 @@ public class TestMain
 
     public static void main(String[] args)
     {
-        double[][] test = Random.generateUniformRandomNumbers(5,10);
-        EPO_MAT.emperorPenguinOptimizer(test, "Test",9, 0,0,0,0,0);
         // Core
         //UnitTest_Agent.doTest();
         //UnitTest_Func.doTest();
@@ -24,9 +20,9 @@ public class TestMain
         //UnitTest_Math.doTest();
 
         // Utils
-        //UnitTest_JNum.doTest();
+        UnitTest_JNum.doTest();
 
         // Optimizers
-        //UnitTest_EPO.doTest();
+        UnitTest_EPO.doTest();
     }
 }
